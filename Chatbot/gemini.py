f9c95a4e43ef2bda0b25b38772af5ebb2516e67b
Malani Snowden <109847401+cxnejxblancx@users.noreply.github.com>
2024-10-23 14:15:06 -0400

    Update gemini.py

--- Chatbot/gemini.py
+++ Chatbot/gemini.py
@@ -6,7 +6,6 @@ Integrate different large language models as global accessible utilities
 2. Install required packages: pip install -r requirements.txt
 3. Assign API key as environment variable: export API_KEY="AIzaSyCrGtK_-qzVe2qhvSNNaYyyx82YlamldtY" ; manual setup for Windows
 
-
 Criteria:
     + Successfully integrate Gemini 1.5 Pro and Gemini 1.5 Flash API
     + Implement error handling and rate limiting
@@ -43,21 +42,20 @@ class GeminiPro:
             cls._instance = super(GeminiPro, cls).__new__(cls) # create new instance of class
             cls._instance.__init__()                           # initialize instance
         return cls._instance                                   # Return single instance of class
-
+    
     def __init__(self):
         # Load API key
         self.api_key = os.environ.get("GEMINI_API_KEY")
         if not self.api_key:
             logging.error("GEMINI_API_KEY is missing from envrionment variables.")
             raise ValueError("GEMINI_API_KEY is undefined.")
-
         # Load Gemini-1.5-Pro model
         try:
             self.model = genai.GenerativeModel("gemini-1.5-pro")
         except Exception as e:
             logging.error(f"Failed to load Gemini-1.5-Pro model: {e}")
             raise RuntimeError("An error occured while loading Gemini-1.5-Pro model.")
-        
+   
     # Set rate limit to 2 RPM
     @sleep_and_retry
     @limits(calls=2, period=60)
@@ -68,10 +66,11 @@ class GeminiPro:
         except Exception as e:
             logging.error(f"Failed to query Gemini-1.5-Pro model: {e}")
             return f"Failed to query Gemini-1.5-Flash model: {e}"
-        
+
+
 class GeminiFlash:
     _instance = None 
-
+    
     # Same pattern as GeminiPro
     def __new__(cls):                                          
         if cls._instance is None:                             
@@ -85,7 +84,6 @@ class GeminiFlash:
         if not self.api_key:
             logging.error("GEMINI_API_KEY is missing from envrionment variables.")
             raise ValueError("GEMINI_API_KEY is undefined")
-
         # Load Gemini-1.5-Pro model
         try:
             self.model = genai.GenerativeModel("gemini-1.5-flash")
@@ -110,5 +108,3 @@ def main():
     q2 = GeminiFlash.query(prompt)
     print(f"Prompt: {prompt}\nGemini Pro: {q1}\nGemini Flash: {q2}")
 main()
-
-        
